c0e0d6888a04a65da0522432f9cd0b0b9b10ee8c
wladim1r <Volodoy53728@mail.ru>
2025-05-11 12:36:31 +0300

    fix

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -5,7 +5,6 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         { "antosha417/nvim-lsp-file-operations", config = true },
         { "folke/neodev.nvim", opts = {} },
-        -- Добавляем mason-lspconfig как зависимость
         "williamboman/mason-lspconfig.nvim",
     },
     config = function()
@@ -13,97 +12,128 @@ return {
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local keymap = vim.keymap
 
-        -- Настройка filetype для templ (если нужно)
-        vim.filetype.add({ extension = { templ = "templ" } })
+        -- Отключаем подчеркивание ошибок
+        vim.cmd([[highlight! link DiagnosticUnderlineError NONE]])
+        vim.cmd([[highlight! link DiagnosticUnderlineWarn NONE]])
+        vim.cmd([[highlight! link DiagnosticUnderlineInfo NONE]])
+        vim.cmd([[highlight! link DiagnosticUnderlineHint NONE]])
 
-        -- Автокоманды для LSP (ваши keymaps остаются без изменений)
+        -- Автокоманды для LSP
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local opts = { buffer = ev.buf, silent = true }
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = { buffer = ev.buf, silent = true }
 
-				-- set keybinds
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+                -- set keybinds
+                opts.desc = "Show LSP references"
+                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+                opts.desc = "Go to declaration"
+                keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+                opts.desc = "Show LSP definitions"
+                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+                opts.desc = "Show LSP implementations"
+                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+                opts.desc = "Show LSP type definitions"
+                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+                opts.desc = "See available code actions"
+                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+                opts.desc = "Smart rename"
+                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+                opts.desc = "Show buffer diagnostics"
+                keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+                opts.desc = "Show line diagnostics"
+                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+                opts.desc = "Go to previous diagnostic"
+                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+                opts.desc = "Go to next diagnostic"
+                keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+                opts.desc = "Show documentation for what is under cursor"
+                keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-			end,
-		})
+                opts.desc = "Restart LSP"
+                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+            end,
+        })
 
-		-- used to enable autocompletion (assign to every lsp server config)
+        -- used to enable autocompletion (assign to every lsp server config)
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
         vim.diagnostic.config({
-          virtual_text = {
-            prefix = "◈",
-            spacing = 4,
-            format = function(diagnostic)
-              return string.format(
-                "%s", 
-                diagnostic.message
-              )            end,
-            -- Показываем только ошибки
-            severity = {
-              min = vim.diagnostic.severity.ERROR,
+            virtual_text = {
+                prefix = "◈",
+                spacing = 2,
+                format = function(diagnostic)
+                    return string.format("%s", diagnostic.message)
+                end,
+                severity = {
+                    min = vim.diagnostic.severity.ERROR,
+                },
             },
-          },
-          
-          -- Обновление диагностики в режиме вставки
-          update_in_insert = true,
+            -- Обновление диагностики в режиме вставки
+            update_in_insert = true,
+            -- Отключаем подчеркивание
+            underline = false,
+            -- Убираем дублирование диагностики
+            -- (проблема могла быть в том, что несколько источников диагностики работали одновременно)
+            virtual_lines = false,
         })
 
-        lspconfig.templ.setup({
-            capabilities = capabilities,
-        })
+        -- Убедитесь, что у вас не включены другие источники диагностики
+        -- Например, null-ls или другие LSP клиенты, которые могут дублировать сообщения
 
         lspconfig.gopls.setup({
             capabilities = capabilities,
+            -- Добавляем настройки для go, чтобы избежать дублирования
+            settings = {
+                gopls = {
+                    analyses = {
+                        unusedparams = true,
+                    },
+                    staticcheck = true,
+                },
+            },
+            -- Отключаем диагностику от других источников
+            on_attach = function(client, bufnr)
+                -- Отключаем другие обработчики диагностики, если они есть
+                client.server_capabilities.documentFormattingProvider = false
+                client.server_capabilities.documentRangeFormattingProvider = false
+            end,
         })
 
         lspconfig.lua_ls.setup({
             capabilities = capabilities,
             settings = {
                 Lua = {
-                    diagnostics = { globals = { "vim" } },
+                    diagnostics = { 
+                        globals = { "vim" },
+                        -- Убедитесь, что не дублируются предупреждения
+                        disable = { "missing-parameters" }
+                    },
                     completion = { callSnippet = "Replace" },
+                    -- Дополнительные настройки для Lua
+                    workspace = {
+                        checkThirdParty = false,
+                    },
                 },
             },
+            -- Аналогично для Lua
+            on_attach = function(client, bufnr)
+                client.server_capabilities.documentFormattingProvider = false
+                client.server_capabilities.documentRangeFormattingProvider = false
+            end,
         })
     end,
 }
