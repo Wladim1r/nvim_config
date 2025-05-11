fbc603db38bf3c770ee30509db2513f634a9c69e
wladim1r <Volodoy53728@mail.ru>
2025-05-11 20:08:00 +0300

    .

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -21,5 +21,6 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
 vim.keymap.set("n", "<C-c>", "<cmd>Telescope close<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>t", ":terminal<CR>i", { desc = "Open terminal and focus" })
 vim.api.nvim_set_keymap('t', 'jj', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
 vim.opt.clipboard = 'unnamedplus'  -- Использовать системный буфер по умолчанию
