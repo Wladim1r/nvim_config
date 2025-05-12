53c19444a99d0563fd9535272b9116a0d006cbcd
wladim1r <Volodoy53728@mail.ru>
2025-05-12 20:34:32 +0300

    triangular bar instead rounded

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,21 +1,21 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
 			options = {
 				theme = "catppuccin",
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				sections = {
 					lualine_a = {
-						{ "mode", separator = { left = "" }, right_padding = 2 },
+						{ "mode", separator = { left = "" }, right_padding = 2 },
 					},
 					lualine_b = { "filename", "branch" },
 					lualine_c = { "fileformat" },
 					lualine_x = {},
 					lualine_y = { "filetype", "progress" },
 					lualine_z = {
-						{ "location", separator = { right = "" }, left_padding = 2 },
+						{ "location", separator = { right = "" }, left_padding = 2 },
 					},
 				},
 			},
