9da179531b5c740045de076423c91586248d57a3
Wladim1r <Volodoy53728@mail.ru>
2025-10-20 22:29:16 +0300

    new terminal shortcut

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,14 +3,14 @@ return {
   version = '*',
   keys = {
     {
-      '<leader>t',
+      '<C-j>',
       '<cmd>ToggleTerm<cr>',
       desc = 'Toggle terminal',
     },
   },
   opts = {
     direction = 'horizontal',
-    open_mapping = [[<leader>t]],
+    open_mapping = [[<C-j>]],
     size = 20,
     shade_terminals = true,
     shading_factor = 2,
